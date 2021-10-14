c0d37bf6fa97394fdb1c6567ef9251e52d70f53a
qinyh <qinyh@seas.upenn.edu>
2021-10-13 20:05:55 -0400

    add about, play, theater pages

--- js/global.js
+++ js/global.js
@@ -3,113 +3,97 @@ var navToggle = 0;
 var tempContent = "<i class='icofont icofont-caret-right'></i>";
 
 
-$("#nav").click(function() {
+$("#nav").click(function () {
   switchMenu();
 });
 
-$("#lang").click(function() {
+$("#lang").click(function () {
   switchMenu();
 });
 
-$("#alt-nav-btn").click(function(){
+$("#alt-nav-btn").click(function () {
   altSwitch();
 })
 
-$("#close").click(function(){
+$("#close").click(function () {
   altClose();
 })
 
-$("#m-alt-nav-btn").click(function(){
+$("#m-alt-nav-btn").click(function () {
   maltSwitch();
 })
 
-$("#m-close-nav").click(function(){
+$("#m-close-nav").click(function () {
   maltClose();
 })
 
-var switchNav = function() {
+var switchNav = function () {
   if (navToggle % 2 == 0) {
     $(".windows-hover-ne").animate({
       right: "-180px"
-    }), 200, "linear", function() {
+    }), 200, "linear", function () {
     }
     $("#nav-toggle").html("<i class='icofont icofont-caret-left'></i>");
   } else {
     $(".windows-hover-ne").animate({
       right: "40px"
-    }), 200, "linear", function() {
+    }), 200, "linear", function () {
     }
     $("#nav-toggle").html("<i class='icofont icofont-caret-right'></i>");
   }
   navToggle += 1;
 }
 
-$("#nav-toggle").click(function() {
+$("#nav-toggle").click(function () {
   switchNav();
 });
 
-$(".option").mouseenter(function() {
+$(".option").mouseenter(function () {
   $(this).css("height", "auto");
   $(this).animate({
     paddingTop: "7.5px",
-  }, 200, "linear", function() {
+  }, 200, "linear", function () {
     $(this).find(".hover-text").show();
     $(this).css("paddingBottom", "10px");
   });
-}).mouseleave(function() {
+}).mouseleave(function () {
   $(this).animate({
     paddingTop: "2.5px",
     paddingBottom: "0px"
-  }, 200, "linear", function() {
+  }, 200, "linear", function () {
     $(this).find(".hover-text").hide();
     $(this).css("paddingBottom", "10px");
     $(this).css("height", "35px");
   });
 });
 
-$("#item-0").mouseenter(function() {
+$("#item-0").mouseenter(function () {
   showDesc(0);
 });
-$("#item-1").mouseenter(function() {
+$("#item-1").mouseenter(function () {
   showDesc(1);
 });
-$("#item-2").mouseenter(function() {
+$("#item-2").mouseenter(function () {
   showDesc(2);
 });
-$("#item-3").mouseenter(function() {
+$("#item-3").mouseenter(function () {
   showDesc(3);
 });
-$("#item-4").mouseenter(function() {
+$("#item-4").mouseenter(function () {
   showDesc(4);
 });
 
-$(window).scroll(function(){
-  if (($(window).scrollTop() > $(window).height() - 90) && (openToggle % 2 == 0)) {
-    // $("#reg").show();
-    // showWithSave("#nav-toggle");
-    // $(".option").css("background-color", "black");
-    $("#alt-nav").show();
-    $("#abs-nav").hide();
-  } else {
-    $("#abs-nav").show();
-    $("#alt-nav").hide();
-    // $("#reg").hide();
-    // hideWithSave("#nav-toggle");
-    // $(".option").css("background-color", "transparent");
-  }
-});
-
-var switchMenu = function(){
+var switchMenu = function () {
   console.log("switchMenu");
- $("#nav").find("i").toggleClass("icofont-navigation-menu");
- $("#nav").find("i").toggleClass("icofont-close");
- $("#menu-container").fadeToggle(300);
- if (openToggle % 2 == 0) {
-   $(".windows-hover-ne").css("background-color", "transparent");
-   $("#lang").hide();
-   hideWithSave("#nav-toggle");
-   $("#reg").hide();
-   $("#nav").find(".hover-text").text("close");
+  $("#nav").find("i").toggleClass("icofont-navigation-menu");
+  $("#nav").find("i").toggleClass("icofont-close");
+  $("#menu-container").fadeToggle(300);
+  if (openToggle % 2 == 0) {
+    $(".windows-hover-ne").css("background-color", "transparent");
+    $("#lang").hide();
+    hideWithSave("#nav-toggle");
+    $("#reg").hide();
+    $("#nav").find(".hover-text").text("close");
   } else {
     $("#lang").show();
     if ($(window).scrollTop() > $(window).height()) {
@@ -122,31 +106,31 @@ var switchMenu = function(){
   openToggle += 1;
 }
 
-var altSwitch = function() {
+var altSwitch = function () {
   $("#menu-container").fadeToggle(300);
   $("#close-nav").show();
   console.log("alt-switch")
 }
 
-var altClose = function() {
+var altClose = function () {
   $("#menu-container").fadeToggle(300);
   $("#close-nav").hide();
   console.log("alt-close")
 }
 
-var maltSwitch = function() {
+var maltSwitch = function () {
   $("#menu-container").fadeToggle(300);
   $("#m-close-nav").show();
   console.log("alt-switch")
 }
 
-var maltClose = function() {
+var maltClose = function () {
   $("#menu-container").fadeToggle(300);
   $("#m-close-nav").hide();
   console.log("alt-close")
 }
 
-var showDesc = function(option) {
+var showDesc = function (option) {
   var options = ["#desc-0", "#desc-1", "#desc-2", "#desc-3", "#desc-4"];
   for (var i = 0; i < options.length(); i++) {
     if (i == option) {
@@ -157,10 +141,10 @@ var showDesc = function(option) {
   }
 }
 
-var disableScroll = function() {
+var disableScroll = function () {
   var scrollPosition = [
     self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-    self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
   ];
   var html = jQuery('html');
   html.data('scroll-position', scrollPosition);
@@ -169,18 +153,18 @@ var disableScroll = function() {
   window.scrollTo(scrollPosition[0], scrollPosition[1]);
 }
 
-var enableScroll = function() {
+var enableScroll = function () {
   var html = jQuery('html');
   var scrollPosition = html.data('scroll-position');
   html.css('overflow', html.data('previous-overflow'));
   window.scrollTo(scrollPosition[0], scrollPosition[1]);
 }
 
-var hideWithSave = function(e) {
+var hideWithSave = function (e) {
   $(e).html("");
 }
 
-var showWithSave = function(e) {
+var showWithSave = function (e) {
   if (navToggle % 2 == 0) {
     $(e).html("<i class='icofont icofont-caret-right'></i>");
   } else {
